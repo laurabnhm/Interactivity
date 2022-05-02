fef768de05b98dc75ce3c9776337cdbd133918da
Candice <candicebranchereau@gmail.com>
2022-05-02 16:20:47 +0200

    add tweets JSON + filter function

--- src/p6/chooseCanvasSize.ts
+++ src/p6/chooseCanvasSize.ts
@@ -6,7 +6,7 @@
 // -------------------
 // Option 1 : Canvas with a fixed aspect ratio
 // -------------------
-const __ASPECT_RATIO = 1
+/* const __ASPECT_RATIO = 1
 const __MARGIN_SIZE = 25 // in pixels
 
 function __desiredCanvasWidth(): number {
@@ -26,42 +26,46 @@ function __desiredCanvasHeight(): number {
     else {
         return windowHeight - __MARGIN_SIZE * 2
     }
-}
+} */
 
 // -------------------
 // Option 2 : Canvas that fills the window
 // -------------------
-// const __MARGIN_SIZE = 0 // in pixels
+const __MARGIN_SIZE = 0  // in pixels
 
-// function __desiredCanvasWidth(): number {
-//     return windowWidth - __MARGIN_SIZE * 2
-// }
-// function __desiredCanvasHeight(): number {
-//     return windowHeight - __MARGIN_SIZE * 2
-// }
+function __desiredCanvasWidth():
+    number{return windowWidth - __MARGIN_SIZE * 2} function
+    __desiredCanvasHeight():
+        number {
+          return windowHeight - __MARGIN_SIZE * 2
+        }
 
 
 // -------------------
 // You don't need to touch the code bellow ;)
 // -------------------
-let __canvas: p5.Renderer // Need to access this each time we resize the window, to center the canvas
+let __canvas: p5.Renderer  // Need to access this each time we resize the
+                           // window, to center the canvas
 
 function __centerCanvas() {
-    __canvas.position((windowWidth - width)/2, (windowHeight - height)/2)
+  __canvas.position((windowWidth - width) / 2, (windowHeight - height) / 2)
 }
 /**
- *   Creates a canvas to start drawing. This is a wrapper around the p5 function createCanvas(w, h) ;
- *   it chooses the size automatically, based on the current option. You can change the option in the p6/chooseCanvasSize.ts file.
+ *   Creates a canvas to start drawing. This is a wrapper around the p5 function
+ * createCanvas(w, h) ; it chooses the size automatically, based on the current
+ * option. You can change the option in the p6/chooseCanvasSize.ts file.
  */
 function p6_CreateCanvas() {
-    __canvas = createCanvas(__desiredCanvasWidth(), __desiredCanvasHeight())
-    __centerCanvas()
+  __canvas =
+      createCanvas(__desiredCanvasWidth(), __desiredCanvasHeight(), WEBGL)
+  __centerCanvas()
 }
 /**
- *   Resizes the canvas. This is a wrapper around the p5 function resizeCanvas(w, h) ;
- *   it chooses the size automatically, based on the current option. You can change the option in the p6/chooseCanvasSize.ts file.
+ *   Resizes the canvas. This is a wrapper around the p5 function
+ * resizeCanvas(w, h) ; it chooses the size automatically, based on the current
+ * option. You can change the option in the p6/chooseCanvasSize.ts file.
  */
 function p6_ResizeCanvas() {
-    resizeCanvas(__desiredCanvasWidth(), __desiredCanvasHeight())
-    __centerCanvas()
+  resizeCanvas(__desiredCanvasWidth(), __desiredCanvasHeight())
+  __centerCanvas()
 }
--- src/sketch.ts
+++ src/sketch.ts
@@ -15,21 +15,109 @@ const params = {
 
 
 // -------------------
-//       Drawing
-// -------------------
-
-function draw() {
-  background('black')
-}
-
-// -------------------
 //    Initialization
 // -------------------
 
+var tweets;
+var filteredTweet = [];
+var author = [
+  'capitalresearch', 'ceidotorg',       'NationalCenter',  'ReasonFdn',
+  'ACSHorg',         'CatoInstitute',   'CFACT',           'FoF_Liberty',
+  'FraserInstitute', 'FreedomWorks',    'HeartlandInst',   'WilcoHeritage',
+  'HudsonInstitute', 'ManhattanInst',   'WAPolicyCente',   'ClimateSkeptic',
+  'ClimateAudit',    'GalileoMovement', 'hockeyschtick1',  'JoanneNova',
+  'JunkScience',     'MasterResource',  'ManicBeancount',  'NOconsensus',
+  'NoTricksZone',    'RogTallbloke',    'wattsupwiththat', 'Fromaway21',
+  'IowaClimate',     'thegwpfcom',      'WEschenbach',     'msroberts0619',
+  'GasPriceWizard',  'MAVERIC22440705', 'BjornLomborg',    'PeterDClack',
+  'socratesccost',   'rglover',         'TreforJones2',    'LukeStPaul1',
+  'KHerriage',       'JaggerMickOZ',    'ClimatePoet',     'ClimateRealists'
+];
+
+// ReasonFdn, ACSHorg, FoF_Liberty, FraserInstitute, WilcoHeritage,
+// HudsonInstitute, ManhattanInst, WAPolicyCente, GalileoMovement,
+// hockeyschtick1, JoanneNova, MasterResource, NOconsensus, thegwpfcom,
+// msroberts0619
+
+function preload() {
+  tweets = loadJSON('../src/tweetszkm_202203.json');
+}
+
 function setup() {
+  // Filter tweets without category
+  for (var i = 0; i < 5364; ++i) {
+    if (tweets[i]['category'] && tweets[i]['category'] != '0_0')
+      filteredTweet.push(tweets[i])
+  }
+  // console.log(filteredTweet)
+
+
+  // Filter tweets according category
+
+  // 1) It's not happening
+  let notHappening = filteredTweet.filter((a) => {
+    if ((a.category).includes('1_')) {
+      return a
+    }
+  });
+  console.log(notHappening)
+
+  // 2) It's not us
+  let notUs = filteredTweet.filter((a) => {
+    if ((a.category).includes('2_')) {
+      return a
+    }
+  });
+  console.log(notUs)
+
+  // 3) It's not bad
+  let notBad = filteredTweet.filter((a) => {
+    if ((a.category).includes('3_')) {
+      return a
+    }
+  });
+  console.log(notBad)
+
+  // 4) Solutions won't work
+  let solutionNotWorking = filteredTweet.filter((a) => {
+    if ((a.category).includes('4_')) {
+      return a
+    }
+  });
+  console.log(solutionNotWorking)
+
+  // 5) Climate science/scientists are unreliable
+  let scienceUnreliable = filteredTweet.filter((a) => {
+    if ((a.category).includes('5_')) {
+      return a
+    }
+  });
+  console.log(scienceUnreliable)
+
+
+  // filter with author
+  for (var i = 0; i < author.length; i++) {
+    let x = filteredTweet.filter((a) => {
+      if (a.author == author[i]) {
+        return a
+      }
+    });
+    // console.log(x)
+  }
+
   p6_CreateCanvas()
 }
 
 function windowResized() {
   p6_ResizeCanvas()
+}
+
+
+// -------------------
+//       Drawing
+// -------------------
+
+function draw() {
+  background(30, 10, 60);
+  blendMode(LIGHTEST)
 }
